52b60c8b6e0a1809f4ee3036626eb6eeceb254e0
Evan Tschuy <tschuy@indent.com>
2022-11-11 02:22:16 -0800

    scripts: noop extra line for testing

--- scripts/idt-okta-jwk.rb
+++ scripts/idt-okta-jwk.rb
@@ -6,3 +6,4 @@ rsa_private = OpenSSL::PKey::RSA.generate 4096
 File.write('./private.pem', rsa_private.to_pem)
 rsa_private = OpenSSL::PKey::RSA.new rsa_private_string
 puts rsa_private.public_key.to_jwk["n"]
+
